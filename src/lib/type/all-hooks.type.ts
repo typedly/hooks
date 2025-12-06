6f61da1514901fd7cbca1b9aecf423aeabd3e200
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-12-06 16:58:41 +0000

    refactor(`AllHooks`, `HookNames`): add `Payload`, and all hooks (and to the `HookNames`) .

--- src/lib/type/all-hooks.type.ts
+++ src/lib/type/all-hooks.type.ts
@@ -1,15 +1,48 @@
 // Interface.
-import { ChangeHook, DestroyHook, SetHook, SetPropertyHook, PropertyChangeHook } from "../interface";
+import {
+  AddHook,
+  AfterAllHook,
+  AfterEachHook,
+  BeforeAllHook,
+  BeforeEachHook,
+  ChangeHook,
+  ClearHook,
+  DestroyHook,
+  GetHook,
+  LockHook,
+  PropertyChangeHook,
+  SetHook,
+  SetPropertyHook,
+  UnlockHook,
+  UpdateHook,
+} from "../interface";
 // Type.
 import { HookNames } from "./hook-names.type";
 /**
  * @description The all hooks type.
  * @export
- * @template {HookNames} K 
+ * @template T The type of value.
+ * @template Payload The type of payload.
+ * @template {HookNames} K The hook names.
+ * @example 
+ * const example: AllHooks<any, {}, 'add' | 'afterEach'> = {}
  */
-export type AllHooks<T, K extends HookNames> = 
-  (K extends 'change' ? ChangeHook<T> : {}) &
-  (K extends 'destroy' ? DestroyHook<T> : {}) &
-  (K extends 'set' ? SetHook<T> : {}) &
+export type AllHooks<T, Payload, K extends HookNames> = 
+  (K extends 'add' ? AddHook<T, Payload> : {}) &
+  (K extends 'afterAll' ? AfterAllHook<T, Payload> : {}) &
+  (K extends 'afterEach' ? AfterEachHook<T, Payload> : {}) &
+  (K extends 'beforeAll' ? BeforeAllHook<T, Payload> : {}) &
+  (K extends 'beforeEach' ? BeforeEachHook<T, Payload> : {}) &
+  (K extends 'change' ? ChangeHook<T, Payload> : {}) &
+  (K extends 'clear' ? ClearHook<T, Payload> : {}) &
+  (K extends 'delete' ? ChangeHook<T, Payload> : {}) &
+  (K extends 'destroy' ? DestroyHook<Payload> : {}) &
+  (K extends 'get' ? GetHook<T, Payload> : {}) &
+  (K extends 'lock' ? LockHook<Payload> : {}) &
   (K extends 'propertyChange' ? T extends object ? PropertyChangeHook<T> : {} : {}) &
-  (K extends 'setProperty' ? T extends object ? SetPropertyHook<T> : {} : {});
+  (K extends 'replace' ? LockHook<Payload> : {}) &
+  (K extends 'set' ? SetHook<T> : {}) &
+  (K extends 'setProperty' ? T extends object ? SetPropertyHook<T> : {} : {}) &
+  (K extends 'unlock' ? UnlockHook<Payload> : {}) &
+  (K extends 'update' ? UpdateHook<T, Payload> : {})
+;
